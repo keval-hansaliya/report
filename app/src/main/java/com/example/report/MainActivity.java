617f30efbc86fd6c7b1546ba1f82c16544844685
Keval <kevalhansaliya1600@gmail.com>
2023-08-20 19:44:05 +0530

    04/08/2023 Date-picker, Radio button validation, Spinner validation

--- app/src/main/java/com/example/report/MainActivity.java
+++ app/src/main/java/com/example/report/MainActivity.java
@@ -6,30 +6,60 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button login;
+    EditText email, password;
+
+    Button login, signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        signup = findViewById(R.id.main_signup_button);
         login = findViewById(R.id.main_login);
+        email = findViewById(R.id.main_emailid);
+        password = findViewById(R.id.main_password);
+
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new CommonMethod(MainActivity.this, SignupActivity.class);
+            }
+        });
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Toast
-                Toast.makeText(MainActivity.this,"Login Successfully", Toast.LENGTH_LONG).show();
-                //Snackbar
-                Snackbar.make(view,"Login Successfully", Snackbar.LENGTH_SHORT).show();
-                //Intent
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(intent);
+                if(email.getText().toString().trim().equals("")){
+                    email.setError("Email Id Required");
+                }
+                else if(!email.getText().toString().trim().matches(emailPattern)){
+                    email.setError("Valid Email Id Required");
+                }
+                else if(password.getText().toString().trim().equals("")){
+                    password.setError("Password Required");
+                }
+                else if(password.getText().toString().trim().length()<6){
+                    password.setError("Min. 6 Char Password Required");
+                }
+                else {
+                    System.out.println("Login Successfully");
+                    //Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_LONG).show();
+                    new CommonMethod(MainActivity.this, "Login Successfully");
+                    //Snackbar.make(view, "Login Successfully", Snackbar.LENGTH_SHORT).show();
+                    new CommonMethod(view,"Login Successfully");
+                    /*Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);*/
+                    new CommonMethod(MainActivity.this, HomeActivity.class);
+                }
             }
         });
     }
