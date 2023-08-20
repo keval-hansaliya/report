1a6f4eb0c7b1095159da569b6140e8331307f446
Keval <kevalhansaliya1600@gmail.com>
2023-08-20 17:42:17 +0530

    01/08/2023 Click event            	Toast and snackbar            	Intent

---
+++ app/src/main/java/com/example/report/CommonMethod.java
@@ -0,0 +1,4 @@
+package com.example.report;
+
+public class CommonMethod {
+}
--- app/src/main/java/com/example/report/MainActivity.java
+++ app/src/main/java/com/example/report/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.report;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.main_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast
+                Toast.makeText(MainActivity.this,"Login Successfully", Toast.LENGTH_LONG).show();
+                //Snackbar
+                Snackbar.make(view,"Login Successfully", Snackbar.LENGTH_SHORT).show();
+                //Intent
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
